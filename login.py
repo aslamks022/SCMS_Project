a51349caececbf5cf0b7bc064eabcbfdb570f49c
Deep Thanush <deepthanush07@gmail.com>
2026-06-28 22:42:10 +0530

    Added login validation

--- login.py
+++ login.py
@@ -3,6 +3,11 @@ username="admin"
 password="1234"
 entered_username=input("Enter Username:")
 entered_password=input("Enter Password:")
+if entered_username == username and entered_password == password:
+    printf("Login Succesful")
+
+
+
 
 
 
