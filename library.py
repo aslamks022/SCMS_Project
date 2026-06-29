c33db24ba30caaab85e0b804f119f4bd9f46fce2
Deep Thanush <deepthanush07@gmail.com>
2026-06-29 11:50:54 +0530

    Added library menu

--- library.py
+++ library.py
@@ -1,3 +1,10 @@
 #Library Management System
 import csv
+def menu():
+    print("1,Add Book")
+    print("2,Search Book")
+    print("3,Issue Book")
+    print("4,Return Book")
+    print("5,Exit")
+
 
