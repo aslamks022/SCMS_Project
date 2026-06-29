005508f46f620515c1a424ef9bdb6cadb9f4a995
Deep Thanush <deepthanush07@gmail.com>
2026-06-29 16:54:58 +0530

    created display_menu function

--- food.py
+++ food.py
@@ -7,4 +7,7 @@ def menu():
     print("4.Exit")
     menu()
     choice = input("Enter your choice:")
-
+    print("1.Idli - 40 rupees")
+    print("2.Dosa - 60 rupees")
+    print("3.Meals - 100 rupees")
+    print("4.Tea - 15 rupees")
