cc8718eb53e62bae5436ba1b4d4f4ef301ef6b46
Deep Thanush <deepthanush07@gmail.com>
2026-06-29 18:35:34 +0530

    created attendance menu

--- attendance.py
+++ attendance.py
@@ -1,4 +1,9 @@
 # Attendance Managemant System
 import csv
 print("==== ATTENDANCE MANAGEMENT SYSTEM ====")
+def menu():
+    print("1.Mark Attendance")
+    print("2.Calculate Attendance")
+    print("3.Display Student Details")
+    print("4.Exit")
 
