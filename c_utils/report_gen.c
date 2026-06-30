04c13f9a784a283c3e3068d4911f0e672769fd8e
S75Deeksha <SriDeeksha75Acharya@gmail.com>
2026-06-30 18:10:57 +0530

    C-118: Report Gen - end function

--- c_utils/report_gen.c
+++ c_utils/report_gen.c
@@ -20,3 +20,4 @@ void generate_student_report(char name[], char usn[], float gpa) {
     fprintf(fp, "============================\n");
     fclose(fp);
     printf("Report generated: report.txt\n");
+}
